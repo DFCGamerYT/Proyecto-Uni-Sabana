81e854f4e62deafb1be7ecfd3c88bc253a1ef270
Andres <acmeneses@unicauca.edu.co>
2026-02-25 20:15:37 -0500

    credenciales sonar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI(title="Api de la Actividad 3 - DevOps")
 
 @app.get("/")
 def read_root():
-    return {"mensaje": "!Bienvenidos a la API optimizada para CI/CD - Prueba Despliegue Automatico 2"}
+    return {"mensaje": "!Bienvenidos a la API optimizada para CI/CD - Prueba Despliegue Automatico 2"} #TODO PRUEBA SONARQUBE, TEST
 
 @app.get("/status")
 def get_status():
