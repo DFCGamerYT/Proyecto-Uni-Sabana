069faf5141663890f5cc332b8a4f8b53deb744bf
David Felipe Cifuentes Villa <DFCGamerYT@gmail.com>
2026-02-26 19:54:47 -0500

    Prueba 2 de despliegue 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI(title="Api de la Actividad 3 - DevOps")
 
 @app.get("/")
 def read_root():
-    return {"mensaje": "!Bienvenidos a la API optimizada para CI/CD - Prueba Despliegue Automatico 2"}
+    return {"mensaje": "!Bienvenidos a la API optimizada para CI/CD - Prueba Despliegue Automatico 3"}
 
 @app.get("/status")
 def get_status():
